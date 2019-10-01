3304361b7159a31f6ec944ab837b6b6c14e1e19e
Alex Mickelson <alexmickelson96@gmail.com>
2019-10-01 16:38:27 -0600

    first commit

---
+++ uptimeCheck.py
@@ -0,0 +1,31 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import time
+import datetime
+from socket import timeout
+
+
+def checkstate(url):
+    state = "up"
+    try:
+        urllib.request.urlopen(url, timeout=1)
+    except (urllib.error.URLError, timeout) as error:
+        if isinstance(error.reason,timeout):
+            print("timeout on: " + url)
+            state="down"
+        else:
+            print("something else happened")
+    return state
+
+def logState(state, lastState):
+    now = datetime.datetime.now()
+    if (state != lastState):
+        print("Time: " + str(now) + " State: " + state)
+
+lastState = ""
+while True:
+    state = checkstate('https://google.com')
+    logState(state, lastState)
+    lastState = state
+    time.sleep(1)
