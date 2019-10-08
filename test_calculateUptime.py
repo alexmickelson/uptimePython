90abdf8c18f39c1fd59fe9c157afcff84db83938
Alex Mickelson <alexmickelson96@gmail.com>
2019-10-07 20:14:12 -0600

    gitignore

---
+++ test_calculateUptime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import unittest
+import uptimeCalculator
+
+
+class uptimeCalculatorTests(unittest.TestCase):
+
+    def test_getuptimeFromSingleList():
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ uptimeCalculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+import stateModel
+
+
+class calculator():
+    def __init__(self, stateCollection: List):
+        stateCollection
+        self.stateCollection = stateCollection
+
